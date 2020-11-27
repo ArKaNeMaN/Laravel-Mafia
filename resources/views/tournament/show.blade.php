ce4401a167942db3b93b29424495958d4cc7408e
ArKaNeMaN <ArKaNeMaN1@gmail.com>
2020-11-27 03:04:45 +0300

    Update show.blade.php

--- resources/views/tournament/show.blade.php
+++ resources/views/tournament/show.blade.php
@@ -9,7 +9,7 @@
     </ul>
     <br>
     <h2>Игры:</h2>
-    @include('game.table', ['games' => $tournament->games()])
+    @include('game.table', ['games' => $tournament->games])
     @role('admin')
     <a href="{{ route('game.create-form', ['tournament_id' => $tournament->id]) }}" class="p-3"><button class="btn btn-primary">Добавить игру</button></a>
     @endrole
