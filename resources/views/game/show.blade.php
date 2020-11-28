6688ee7ccf3d220a43ee0c2592961ef72f7f412f
ArKaNeMaN <ArKaNeMaN1@gmail.com>
2020-11-28 20:58:58 +0300

    Fix game page

--- resources/views/game/show.blade.php
+++ resources/views/game/show.blade.php
@@ -12,7 +12,14 @@
     <ul class="list-group col-md-4">
         <li class="list-group-item"><b>#</b>{{ $game->id }}</li>
         <li class="list-group-item"><b>Игровой день:</b> <a href="{{ route('day.show', ['day' => $game->day]) }}">{{ $game->day->day_date }}</a></li>
-        <li class="list-group-item"><b>Турнир:</b> <a href="{{ route('tournament.show', ['tournament' => $game->tournament]) }}">{{ $game->tournament->id }}</a></li>
+        <li class="list-group-item">
+            <b>Турнир: </b>
+            @if ($game->tournament)
+                <a href="{{ route('tournament.show', ['tournament' => $game->tournament]) }}">{{ $game->tournament->id }}</a>
+            @else
+                -
+            @endif
+        </li>
         <li class="list-group-item"><b>Ведущий:</b> <a href="{{ route('player.show', ['player' => $game->leader]) }}">{{ $game->leader->nickname }}</a></li>
         @if ($game->bestRed)
             <li class="list-group-item"><b>Лучший красный:</b> <a href="{{ route('player.show', ['player' => $game->bestRed]) }}">{{ $game->bestRed->nickname }}</a></li>
