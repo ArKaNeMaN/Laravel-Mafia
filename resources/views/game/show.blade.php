fa1b6515dcb7dfc07ead030e6e9535fd7ef346d0
ArKaNeMaN <ArKaNeMaN1@gmail.com>
2020-12-01 23:32:13 +0300

    Add nights table to game.show

--- resources/views/game/show.blade.php
+++ resources/views/game/show.blade.php
@@ -37,4 +37,14 @@
     @role('admin')
     <a href="{{ route('game.player.create-form', ['game_id' => $game->id]) }}" class="p-3"><button class="btn btn-primary">Добавить участника</button></a>
     @endrole
+    <br>
+
+    <div class="d-flex justify-content-around flex-column flex-lg-row">
+        <div class="col-lg-6">
+            <h2>Ночи:</h2>
+            @include('game.night.table', ['nights' => $game->nights, 'hideGame' => '1'])
+            @role('admin')
+            <a href="{{ route('game.night.create-form', ['game_id' => $game->id]) }}" class="p-3"><button class="btn btn-primary">Добавить ночь</button></a>
+            @endrole
+        </div>
 @endsection
