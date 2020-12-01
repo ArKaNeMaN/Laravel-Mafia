ef50210934cb6763ca82ef65198e4c87a348292b
ArKaNeMaN <ArKaNeMaN1@gmail.com>
2020-12-01 21:45:33 +0300

    Update GameFactory.php
    Add generating nights for game

--- database/factories/GameFactory.php
+++ database/factories/GameFactory.php
@@ -6,6 +6,7 @@ use App\Models\Day;
 use App\Models\Player;
 use App\Models\Game;
 use App\Models\GamePlayer;
+use App\Models\GameNight;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class GameFactory extends Factory
@@ -39,6 +40,7 @@ class GameFactory extends Factory
     public function configure()
     {
         return $this->afterCreating(function (Game $game) {
+            // Create players
             $players = Player::where('id', '!=', $game->leader_id)->inRandomOrder()->limit(10)->get();
             $roles = $this->faker->shuffle(['black', 'black', 'don', 'sheriff', 'red', 'red', 'red', 'red', 'red', 'red']);
             for($i = 1; $i <= 10; $i++)
@@ -46,7 +48,18 @@ class GameFactory extends Factory
                     'game_id' => $game->id,
                     'player_id' => $players[$i-1]->id,
                     'role' => $roles[$i-1],
-                    //'ingame_player_id' => $i,
+                    'ingame_player_id' => $i,
+                ])->create();
+
+            // Create nights
+            $players = GamePlayer::where('game_id', $game->id)->inRandomOrder()->get();
+            for($i = 1; $i <= 5; $i++)
+                GameNight::factory()->state([
+                    'game_id' => $game->id,
+                    'ingame_id' => $i,
+                    'killed_id' => $players[random_int(0, count($players)-1)]->id,
+                    'checked_don_id' => $players[random_int(0, count($players)-1)]->id,
+                    'checked_sheriff_id' => $players[random_int(0, count($players)-1)]->id,
                 ])->create();
         });
     }
