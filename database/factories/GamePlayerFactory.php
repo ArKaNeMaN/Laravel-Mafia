3d1f7e047a69cb33c3fa944f89f901dc3694587a
ArKaNeMaN <ArKaNeMaN1@gmail.com>
2020-12-01 21:42:51 +0300

    Update GamePlayerFactory.php

--- database/factories/GamePlayerFactory.php
+++ database/factories/GamePlayerFactory.php
@@ -26,14 +26,17 @@ class GamePlayerFactory extends Factory
     public function definition()
     {
         $game = Game::inRandomOrder()->first();
+        $player = Player::inRandomOrder()->first();
+
         if(!isset(self::$gamesPlayersCounter[$game->id]))
             self::$gamesPlayersCounter[$game->id] = 1;
-        $player = Player::inRandomOrder()->first();
+        else self::$gamesPlayersCounter[$game->id]++;
+
         return [
             'game_id' => $game->id,
             'player_id' => $player->id,
             'helper_id' => random_int(1, 25) == 1 ? Player::where('id', '!=', $player->id)->inRandomOrder()->first()->id : null,
-            'ingame_player_id' => self::$gamesPlayersCounter[$game->id]++,
+            'ingame_player_id' => self::$gamesPlayersCounter[$game->id],
             'role' => $this->faker->randomElement(['black', 'black', 'don', 'sheriff', 'red', 'red', 'red', 'red', 'red', 'red']),
         ];
     }
