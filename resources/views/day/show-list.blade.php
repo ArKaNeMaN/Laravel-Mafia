b23a68e8c527bd325ed3949ed6641d99637f983e
ArKaNeMaN <ArKaNeMaN1@gmail.com>
2020-12-03 03:07:36 +0300

    Add view `cards` for days

--- resources/views/day/show-list.blade.php
+++ resources/views/day/show-list.blade.php
@@ -3,7 +3,7 @@
 @section('breadcrumbs', Breadcrumbs::render('days'))
 
 @section('content')
-@include('day.table', ['days' => $days])
+@include('day.cards', ['days' => $days])
 @role('admin')
 <a href="{{ route('day.create-form') }}" class="p-3"><button class="btn btn-primary">Добавить игровой день</button></a>
 @endrole
