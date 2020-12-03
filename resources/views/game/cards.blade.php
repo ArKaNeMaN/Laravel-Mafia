730380b724d6a3d0fedcf0afaf9c4fad501e040f
ArKaNeMaN <ArKaNeMaN1@gmail.com>
2020-12-03 03:07:30 +0300

    Add view `cards` for games

---
+++ resources/views/game/cards.blade.php
@@ -0,0 +1,43 @@
+<div class="d-flex flex-wrap flex-column align-content-stretch">
+    @foreach ($games as $g)
+    <div class="card shadow m-2">
+        <div class="card-header">
+            Игра №{{ $g->id }}
+            <span class="{{ $g->result == 'red_win' ? 'bg-danger' : 'bg-dark' }} float-right text-monospace px-1 rounded-sm">{{ __('mafia.result-'.$g->result) }}</span>
+        </div>
+        <div class="card-body d-md-flex">
+            <div class="mr-md-5">
+                <p><b>Дата: </b><a href="{{ route('day.show', ['day' => $g->day]) }}">{{ $g->day->day_date }}</a></p>
+                @if($g->tournament)
+                    <p><b>Турнир: </b><a href="{{ route('tournament.show', ['tournament' => $g->tournament]) }}">{{ $g->tournament->name }}</a></p>
+                @endif
+                <p class="card-text">{{ $g->description }}</p>
+            </div>
+            <div class="mr-md-5">
+                <p><b>Ведущий: </b><a href="{{ route('player.show', ['player' => $g->leader]) }}">{{ $g->leader->nickname }}</a></p>
+                <p><b>Кол-во участников: </b>{{ $g->players->count() }}</p>
+            </div>
+            <div class="mr-md-5">
+                @if($g->bestRed)
+                    <p><b>Лучший красный: </b><a href="{{ route('player.show', ['player' => $g->bestRed]) }}">{{ $g->bestRed->nickname }}</a></p>
+                @endif
+                @if($g->bestBlack)
+                    <p><b>Лучший чёрный: </b><a href="{{ route('player.show', ['player' => $g->bestBlack]) }}">{{ $g->bestBlack->nickname }}</a></p>
+                @endif
+            </div>
+        </div>
+        <div class="card-footer">
+            <div class="card-text float-right">
+                @role('admin')
+                <a href="{{ route('game.edit-form', ['game' => $g]) }}" class="card-link">
+                    <button type="button" class="btn btn-primary btn-sm">Изменить</button>
+                </a>
+                @endrole
+                <a href="{{ route('game.show', ['game' => $g]) }}" class="card-link">
+                    <button type="button" class="btn btn-primary btn-sm">Подробнее...</button>
+                </a>
+            </div>
+        </div>
+    </div>
+    @endforeach
+</div>
--- resources/views/game/show-list.blade.php
+++ resources/views/game/show-list.blade.php
@@ -3,7 +3,7 @@
 @section('breadcrumbs', Breadcrumbs::render('games'))
 
 @section('content')
-@include('game/table', ['games' => $games])
+@include('game.cards', ['games' => $games])
 @role('admin')
 <a href="{{ route('game.create-form') }}" class="p-3"><button class="btn btn-primary">Добавить игру</button></a>
 @endrole
